3c6c022369bd77c0f5ba0c406423bd3aa913df83
Andrewtho5942 <andrewtho5942@gmail.com>
2024-11-26 15:51:22 -0600

    add simple input form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [link, setLink] = useState('')
+
+  const updateLink = (e) => {
+    let newText = e.target.value;
+    console.log('updated link to: '+newText);
+    setLink(newText);
+  }
+
+  const submitLink = () => {
+    console.log('submitted link: ' + link);
+    setLink('');
+  }
+
+
   return (
     <div className="App">
-      test text
+      <input type="text" value={link} onChange={updateLink}></input>
+      <button onClick={()=>{submitLink()}}>submit</button>
     </div>
   );
 }
