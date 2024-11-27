25f910edb0fc71a147cbed9b2557b5fe521b2903
Andrewtho5942 <andrewtho5942@gmail.com>
2024-11-26 18:53:42 -0600

    Make it work and look good

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import './App.css';
-import { useState } from 'react';
+import React, { useState } from 'react';
+import download from '../public/dl.png';
 
 function App() {
-  const [link, setLink] = useState('')
+  const [link, setLink] = useState('');
 
   const updateLink = (e) => {
     let newText = e.target.value;
-    console.log('updated link to: '+newText);
+    console.log('updated link to: ' + newText);
     setLink(newText);
-  }
+  };
 
   const submitLink = () => {
     console.log('submitted link: ' + link);
     setLink('');
-  }
-
+  };
 
   return (
-    <div className="App">
-      <input type="text" value={link} onChange={updateLink}></input>
-      <button onClick={()=>{submitLink()}}>submit</button>
+    <div className="App" >
+      <div className="header-container">
+        <img src={download} alt="YT image" className="dl-img"></img>
+        <h1>YT Downloader</h1>
+      </div>
+
+      <input
+        type="text"
+        value={link}
+        onChange={updateLink}
+        placeholder="Enter YouTube URL"
+      />
+      <button onClick={submitLink}>Download</button>
     </div>
   );
 }
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js'],
+    alias: {
+      react: require.resolve('react'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i, 
+        type: 'asset/resource', 
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    port: 3000,
+    open: true,
+  },
+};
